5c905fd9e1feca6cc8b104be7c5acfe2c5fa4510
Kevin Anderson Santos Lopez <kevin.santos@grupoemco.com>
2025-07-29 23:13:52 -0600

    mostsales

--- src/api/mostsales.ts
+++ src/api/mostsales.ts
@@ -1,7 +1,7 @@
 import axios from "./axiosInstance";
-import type { Product } from "../types/product";
+import type { MostSale } from "../types/MostSales";
 
-export const fetchMostSales = async(): Promise<Product[]> => {
+export const fetchMostSales = async(): Promise<MostSale[]> => {
     const res = await axios.get('/sales/most_sales_now');
     console.log("Datos recibidos:", res.data);
     return res.data
